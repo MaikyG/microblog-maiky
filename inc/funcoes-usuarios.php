65eb968ef87c5e56f27123a7e2740d8ac5716100
MaikyG <maikygabriel3@gmail.com>
2022-05-26 16:57:36 -0300

    atualizar

--- inc/funcoes-usuarios.php
+++ inc/funcoes-usuarios.php
@@ -40,13 +40,20 @@ function lerUsuarios( mysqli $conexao){
 
 
 // Função lerUmUsuario: usada em usuario-atualiza.php
-
+function lerUmUsuario(mysqli $conexao, int $id):array{
+    $sql = "SELECT id,nome,email,senha,tipo FROM usuarios WHERE id = $id";
+    $resultado =  mysqli_query($conexao, $sql) or die(mysqli_error($conexao));
+    return mysqli_fetch_assoc($resultado);
+}
 // fim lerUmUsuario
 
 
 
 // Função verificaSenha: usada em usuario-atualiza.php
-
+function atualizarUsuario(mysqli $conexao, int $id, string $nome, string $email, string $senha, string $tipo){
+    $sql = "UPDATE usuarios SET nome='$nome', email='$email',senha='$senha',tipo='tipo' WHERE id = $id";
+    mysqli_query($conexao, $sql) or die(mysqli_error($conexao));
+}
 // fim verificaSenha
 
 
