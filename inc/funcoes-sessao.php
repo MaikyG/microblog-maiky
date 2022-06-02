6bba7c1e25390a8145fc36badfc730a87d9d721c
MaikyG <maikygabriel3@gmail.com>
2022-06-02 14:08:10 -0300

    logout

--- inc/funcoes-sessao.php
+++ inc/funcoes-sessao.php
@@ -29,6 +29,6 @@ function login(int $id, string $nome, string $email, string $tipo){
 function logout(){
     session_start();
     session_destroy();
-    header("location:../login.php");
+    header("location:../login.php?logout");
     die();
 }
--- login.php
+++ login.php
@@ -3,19 +3,40 @@ require "inc/cabecalho.php";
 require "inc/funcoes-sessao.php";
 require "inc/funcoes-usuarios.php";
 
+/* Mensagens para os processos de erros no login */
+if( isset($_GET['acesso_proibido']) ){
+  $feedback = "Você deve logar primeiro!";
+} elseif( isset($_GET['logout']) ) {
+  $feedback = "Você saiu do sistema!";
+} elseif( isset($_GET['nao_encontrado']) ) {
+  $feedback = "Usuário não encontrado!";
+} elseif( isset($_GET['senha_incorreta']) ) {
+  $feedback = "A senha está errada!";          
+} elseif( isset($_GET['campos_obrigatorios']) ) {
+  $feedback = "Você deve preencher todos os campos!";
+} else {
+  $feedback = "";
+}
+
+//se o botão foi acionado
 if (isset($_POST['entrar'])){
-  if( empty($_POST['email']) || empty($_POST['senha'])) {
+//se os campos estão vazios
+  if( empty($_POST['email']) || empty($_POST['senha'])){
+//redireciona para o login com parametro indicando campos obrigatorios
    header("location:login.php?campos_obrigatorios");
   } else {
+//caso contrario, pegue o email e a senha digitadas
     $email = filter_input(INPUT_POST,'email', FILTER_SANITIZE_EMAIL);
     $senha = $_POST['senha'];
 
     /* verificando no banco de existe alguma com email informado */
-
     $usuario = buscarUsuario($conexao,$email);
-    
+
+//3) [if/else] se usuarioé diferente de nulo (ou seja, se tem usuario)
     if ($usuario != null) {
+//4) [if/else] se as senhas forem iguais
       if(password_verify($senha, $usuario['senha'])){
+//então inicia o login para a área administrativa
         login($usuario['id'],$usuario['nome'],$usuario['email'],$usuario['tipo']);
         header("location:admin/index.php");
 
@@ -23,7 +44,7 @@ if (isset($_POST['entrar'])){
         header("location:login.php?senha_incorreta");
       }
     }else{
-      header("location:login.php?nao_encotrado");
+      header("location:login.php?nao_encontrado");
     }
   }
 }
@@ -36,7 +57,7 @@ if (isset($_POST['entrar'])){
     <form action="" method="post" id="form-login" name="form-login" class="mx-auto w-50">
 
       <p class="my-2 alert alert-warning text-center">
-        Mensagem...
+        <?=$feedback?>
       </p>
 
       <div class="form-group">
