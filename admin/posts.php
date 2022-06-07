87f0173df4c82bfe78927b34883f3d1ddd67ecf3
MaikyG <maikygabriel3@gmail.com>
2022-06-07 16:52:02 -0300

    funoes posts e posts

--- admin/posts.php
+++ admin/posts.php
@@ -1,11 +1,20 @@
 <?php 
-require "../inc/cabecalho-admin.php"; 
+require "../inc/funcoes-posts.php";
+require "../inc/cabecalho-admin.php";
+
+$idUsuarioLogado = $_SESSION['id'];
+$tipoUsuarioLogado = $_SESSION['tipo'];
+
+$posts = lerPosts( $conexao, $idUsuarioLogado, $tipoUsuarioLogado);
+$quantidade = count ($posts);
+
+
 
 ?>      
     
 <div class="row">
   <article class="col-12 bg-white rounded shadow my-1 py-4">
-    <h2 class="text-center">Posts <span class="badge badge-primary">X</span></h2>
+    <h2 class="text-center">Posts <span class="badge badge-primary"><?=$quantidade?></span></h2>
     <p class="lead text-right">
       <a class="btn btn-primary" href="post-insere.php">Inserir novo post</a>
     </p>
@@ -17,18 +26,23 @@ require "../inc/cabecalho-admin.php";
           <tr>
             <th>Título</th>
             <th>Data</th>
+            <?php if($tipoUsuarioLogado == 'admin'){ ?> 
             <th>Autor</th>
+             <?php }; ?>
             <th colspan="2" class="text-center">Operações</th>
           </tr>
         </thead>
       
         <tbody>
-
+ <?php foreach($posts as $postes){?>
           <tr>
-            <td> Título do post... </td>
-            <td> 21/12/2112 21:12 </td>
-            <td> Autor do post... </td>
+            <td> <?=$postes['titulo']?> </td>
+            <td> <?=$postes['data']?> </td>
+<?php if($tipoUsuarioLogado == 'admin'){ ?> 
+            <td> <?=$postes['autor']?> </td>
+<?php }; ?>
             <td class="text-center">
+
               <a class="btn btn-warning btn-sm" 
               href="post-atualiza.php">
                   Atualizar
@@ -41,7 +55,7 @@ require "../inc/cabecalho-admin.php";
               </a>
             </td>
           </tr>
-
+ <?php }; ?>
         </tbody>                
       </table>
       
